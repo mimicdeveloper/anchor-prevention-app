ea9ed2efd27a081204218452eb5f9915619acad7
Yon Chong <yonchong31@gmail.com>
2025-07-30 00:13:27 -0400

    updated router in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom'; // no BrowserRouter here
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -8,25 +8,23 @@ import Register from './components/Register';
 import Forum from './components/Forum';
 import MoodTracker from './components/MoodTracker';
 import CrisisResources from './components/CrisisResources';
-import Footer from './components/Footer';  // <-- import Footer here
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <div style={{ minHeight: '100vh', position: 'relative', paddingBottom: '50px' }}>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forum" element={<Forum />} />
-            <Route path="/mood" element={<MoodTracker />} />
-            <Route path="/crisis-resources" element={<CrisisResources />} />
-          </Routes>
-        </Layout>
-        <Footer />  {/* <-- Add Footer here */}
-      </div>
-    </Router>
+    <div style={{ minHeight: '100vh', position: 'relative', paddingBottom: '50px' }}>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/mood" element={<MoodTracker />} />
+          <Route path="/crisis-resources" element={<CrisisResources />} />
+        </Routes>
+      </Layout>
+      <Footer />
+    </div>
   );
 }
 
