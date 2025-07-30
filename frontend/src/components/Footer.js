380d962a4ad46312313fc5dd0e84492e170682af
Yon Chong <yonchong31@gmail.com>
2025-07-30 15:07:53 -0400

    updated footer to span across the bottom of app on mobile

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -11,10 +11,12 @@ function Footer() {
         fontSize: '0.9rem',
         position: 'fixed',
         bottom: 0,
-        width: '100%',
+        left: 0,
+        right: 0,
         boxShadow: '0 -2px 5px rgba(0,0,0,0.1)',
         fontFamily: 'Poppins, sans-serif',
         userSelect: 'none',
+        zIndex: 1000,
       }}
     >
       &copy; 2025 Anchor Prevention
